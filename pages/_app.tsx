f9ed76319196aa2a178e48f9c0bbe1e52673b932
Neo Wie <it.brandonwie@gmail.com>
2021-04-30 13:52:03 +0800

    fix(pages/_document.tsx): error message does not appear

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import Layout from '../layout/layout';
@@ -8,24 +8,30 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../styles/theme';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  <Head>
-    <title>Next and Typescript</title>
-    <meta
-      name="viewport"
-      content="minimum-scale=1, initial-scale=1, width=device-width"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-    />
-  </Head>;
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles?.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>Next and Typescript</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
   );
 };
 
