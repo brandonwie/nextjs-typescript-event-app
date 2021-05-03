95942bd598c28ce8217d83e72e1992433789eede
Neo Wie <it.brandonwie@gmail.com>
2021-05-04 04:23:47 +0800

    add(events/[...slug]): managing parameters dynamically

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -1,4 +1,44 @@
-const FilteredEventsPage: React.FC = (): JSX.Element => {
+import { FC } from 'react';
+import { NextRouter, useRouter } from 'next/router';
+import { getFilteredEvents } from '../../dummy-data';
+
+const FilteredEventsPage: FC = (): JSX.Element => {
+  const router: NextRouter = useRouter();
+
+  const filterData = router.query.slug;
+  // slug is an array of param items
+  console.log(filterData);
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values.</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter!</p>;
+  }
+
   return (
     <div>
       <h1>Filtered Events</h1>
